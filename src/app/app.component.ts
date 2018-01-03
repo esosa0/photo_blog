b8792187d2be0d99f14dd87d0a42581cdfb81ce5
Erica Sosa <erica.sosa@getbraintree.com>
2018-01-03 13:44:41 -0600

    extracted template and styles from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h2>Hello World!</h2>`
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'] 
 })
 
 export class AppComponent {
