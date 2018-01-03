5723b660f1f5ca7177ed27ec6f28cf04febfd365
Erica Sosa <erica.sosa@getbraintree.com>
2018-01-03 17:50:56 -0600

    added likes and comments

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
 export class EntryComponent {
   title: string = 'My First Photo';
   photo: string = 'http://placehold.it/800x500?text=Angular Basics';
-  description: string = 'A Description of My First Photo'
+  description: string = 'A Description of My First Photo';
+  comments: any[] = [
+    {name: "John", comment: "A comment"},
+    {name: "Jim", comment: "A comment"},
+    {name: "Jen", comment: "A comment"}
+  ]
 }
